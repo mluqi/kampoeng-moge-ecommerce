636522ca8463b141fa8f68082e2951299cf17e19
mluqi <mluqi12@gmail.com>
2025-06-20 16:55:42 +0700

    add setup frontend/backend

---
+++ kamo-ecommerce/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppinsFont = Poppins({
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata: Metadata = {
+  title: "Kampoeng Moge",
+  description: "Kampoeng Moge E-commerce",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${poppinsFont.variable} ${poppinsFont.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
